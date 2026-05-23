45190d452e7c883dab90a0182de7182473536468
aandrzejmikulski-lang <aandrzej.mikulski@gmail.com>
2026-05-23 19:24:58 +0200

    Initialize Supabase client with configuration

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,7 @@
+import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
+
+export const supabase = createBrowserClient(
+  "https://owhxyoxswzcycmgluywv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93aHh5b3hzd3pjeWNtZ2x1eXd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NDg0MDEsImV4cCI6MjA5NTEyNDQwMX0.WV38FzTFmgnJWpe5QHDA5Gp9UgTeb3j8ldh5z9LGHcI"
+);
 
